b673938e60c35b5d83eb049287394b4b12a1d143
tylerzhou <tylerzhou@foxmail.com>
2022-06-29 16:55:47 +0800

    axios增加post方法

--- src/axios/index.js
+++ src/axios/index.js
@@ -19,4 +19,21 @@ export function get ( url, params ) {
       } )
   } )
 }
+/**
+ * post方法，对应post请求
+ * @param {String} url [请求的url地址]
+ * @param {Object} params [请求时携带的参数]
+ */
+export function post(url,params){
+  params = params || {}
+  return new Promise( ( resolve, reject ) => {
+    axios
+        .post(url,params)
+        .then(res=>{
+          resolve(res.data)
+        }).catch(err=>{
+      reject(err.data())
+    })
+  })
+}
 export default axios
